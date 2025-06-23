07776188f2a87da94bdb3f87e278da3103453305
Kassi_0958 <marvinjunior0958@gmail.com>
2025-06-23 05:08:58 +0200

    feat: mises à jour de l'interface et de l'authentification
    - Changement d'image dans le héros
    - Suppression de l'image dans la section église
    - Réorganisation des composants dans la page principale
    - Correction de l'authentification avec exports nommés

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,9 +27,10 @@ export default function Campaign() {
       <Feature />
       <ChurchSection />
       {/* <Testimonials /> */}
-      <Faqs />
       <Departments />
       <BlogSection />
+      <Faqs />
+
     </>
   );
 }
