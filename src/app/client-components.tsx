bfac52a41cc3290cb7f0424549681261db235668
Kassi_0958 <marvinjunior0958@gmail.com>
2025-07-09 10:17:28 +0200

    Update video URLs in FirstVisitModal

--- src/app/client-components.tsx
+++ src/app/client-components.tsx
@@ -6,13 +6,12 @@ import PastorMessage from "@/components/pastor-message";
 export function ClientFirstVisitModal() {
   return (
     <FirstVisitModal videoUrls={[
-      "https://www.facebook.com/pasteurhermanntano/videos/651434039959985",
-      "https://www.facebook.com/pasteurhermanntano/videos/725174849362205",
+      "https://www.facebook.com/pasteurhermanntano/videos/1545923519460317",
+      "https://www.facebook.com/pasteurhermanntano/videos/1642662756550642",
       "https://www.facebook.com/pasteurhermanntano/videos/670268458970941",
-      "https://www.facebook.com/pasteurhermanntano/videos/725174849362205",
-      "https://www.facebook.com/pasteurhermanntano/videos/651434039959985",
-      "https://www.facebook.com/pasteurhermanntano/videos/670268458970941",
-      "https://www.facebook.com/pasteurhermanntano/videos/725174849362205"
+      "https://www.facebook.com/pasteurhermanntano/videos/1020856390034517",
+      
+
     ]} />
   );
 }
